cc464a068168156d2f32f27a30d1bdfe39cde55a
Raushanroy25 <raushanroy541@gmail.com>
2023-07-19 14:38:17 +0530

    commit

--- Practice.js/1.js
+++ Practice.js/1.js
@@ -14,15 +14,21 @@
 
 // console.log(nums);
 
-function sayName() {
-  name = {
-    name: "Raushan",
-    last_Name: "Roy",
-  };
-  return name.last_Name;
-}
-function greeting(myName) {
-  var myName = sayName("Ifeoma");
-  console.log(`Hello ${myName}`);
-}
-greeting();
+// function sayName() {
+//   name = {
+//     name: "Raushan",
+//     last_Name: "Roy",
+//   };
+//   return name.last_Name;
+// }
+// function greeting(myName) {
+//   var myName = sayName("Ifeoma");
+//   console.log(`Hello ${myName}`);
+// }
+// greeting();
+
+let a = "Age";
+let person = {
+  [a]: 30,
+};
+console.log(person);
---
+++ Practice.js/Two.js
@@ -0,0 +1,26 @@
+// filter
+
+let student = [
+  { id: "01", name: "Raushan", age: 23, sport: "cricket" },
+  { id: "02", name: "Golu", age: 20, sport: "football" },
+  { id: "03", name: "Prabhash", age: 22, sport: "Hockey" },
+  { id: "04", name: "Raja", age: 23, sport: "basketBall" },
+  { id: "05", name: "Vikash", age: 23, sport: "cricket" },
+];
+
+// const newStudent = student.filter((currentVall) => {
+//   if (currentVall.id % 2 == 0) return true;
+//   else false;
+// });
+// console.log(newStudent);
+
+// map method
+const newStudent = student
+  .filter((currVall) => {
+    if (currVall.sport === "Hockey") return true;
+    else false;
+  })
+  .map((currVall) => {
+    return currVall.name || currVall.age === 20;
+  });
+console.log(newStudent);
